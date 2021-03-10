6a8190d7e5e3a3bdac8a189ee993caabfd49e3ae
Anthony DeCenzo <9rarwolf9@gmail.com>
2021-03-10 12:51:35 -0500

    Added animations and may have merged other stuff

---
+++ Error Drive/ErrorDrive/Assets/Rotate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+
+    public Transform target;
+    public Component test;
+    public Rigidbody test2;
+
+
+    void Update()
+    {
+        // Rotate the camera every frame so it keeps looking at the target
+        transform.LookAt(target);
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Destroy(test);
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            test2.useGravity = true;
+            test2.AddForce(transform.up * 150.0f);
+            Destroy(this);
+        }
+
+    }
+
+
+
+
+
+
+
+}
